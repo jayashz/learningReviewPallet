51f6c7e542cfca9378a0f70bf3cd5402e5c77684
jayashz <jayzstha15@gmail.com>
2023-07-06 21:45:54 +0545

    added the functionality of next and previous button

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Local review data
-const reviews = [
+const review = [
     {
       id: 1,
       name: 'susan smith',
@@ -31,15 +31,51 @@ const reviews = [
   ];
 
 //selecting items
-const img= document.getElementById("");
+const img= document.getElementById("person-img");
 const author= document.getElementById("author");
 const job= document.getElementById("job");
 const info= document.getElementById("info");
 
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
-const ramdomBtn = document.querySelector(".random-btn");
+const randomBtn = document.querySelector(".random-btn");
 
 //set starting item
-let currentItem=0;
- 
+let currentItem = 0;
+
+//loading the initial item
+window.addEventListener('DOMContentLoaded',function(){
+  showPerson[currentItem];
+});
+
+//showing person based on item
+function showPerson(person){
+  const item= review[person];
+  img.src= item.img;
+  author.textContent= item.name;
+  job.textContent= item.job;
+  info.textContent=item.text;
+}
+
+//showing the next person
+nextBtn.addEventListener('click',function(){
+  currentItem++;
+  if(currentItem>review.length-1){
+    currentItem=0;
+  }
+  showPerson(currentItem);
+});
+//showing the previous person
+prevBtn.addEventListener('click',function(){
+  currentItem--;
+  if(currentItem<review.length-1){
+    currentItem=review.length-1;
+  }
+  showPerson(currentItem);
+});
+//showing any random person
+randomBtn.addEventListener('click',function(){
+  currentItem= math.floor(math.random()*4);
+  showPerson[currentItem];
+  
+});
